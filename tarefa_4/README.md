bc01861e64da567f9e62729dbd92b533eac2a28c
christiandpo <christiandpo@gmail.com>
2022-03-23 20:02:48 -0300

    - Added final script to create distribution graph - Added graph for chosen dataset in tarefa_4/results - Updated tarefa_4/README.md

--- tarefa_4/README.md
+++ tarefa_4/README.md
@@ -1,15 +1,23 @@
 # Vetor de características e Distribuição do conjunto de dados
 
-## Atributos selecionados
-- 
--
-- 
+- Os resultados dos esperimentos dessa tarefa foram adicionados ao diretório `results/`
 
-## Fontes/guia de conteúdos
+## Scripts
+`$create_distribuition_graph.py `
+- __Descrição__: Cria um grafico de barras que mostra a distribuição de amostras por classe (quantidade de exemplos para cada classe)
+- __Execução__: `python3 create_distribuition_graph.py <csv_file> <class_column_name>`
+    - `<csv_file>`: Path para o arquivo CSV do dataset para plotar o gráfico
+    - `<class_column_name>`: nome da coluna do CSV que contém as labels das classes de cada exemplo
+- __Exemplo__:
+    - `python3 create_distribuition_graph.py ../../tarefa_3/dataset/FinalDataset/All.csv URL_Type_obf_Type`
+
+
+## Fontes/Guia de conteúdos
 
 - https://jakevdp.github.io/PythonDataScienceHandbook/04.02-simple-scatter-plots.html
 - https://matplotlib.org/3.5.1/api/_as_gen/matplotlib.pyplot.scatter.html
 - https://matplotlib.org/3.5.1/gallery/shapes_and_collections/scatter.html#sphx-glr-gallery-shapes-and-collections-scatter-py
+- https://pandas.pydata.org/docs/reference/api/pandas.plotting.scatter_matrix.html
 
 
 
--- tarefa_4/src/create_distribuition_graph.py
+++ tarefa_4/src/create_distribuition_graph.py
@@ -1,34 +1,25 @@
 #!/usr/bin/env python
 """
 Script responsável para plotar um grafico de barras com a distribuição dos dados de um dataset
-
-Este script foi feito para funcionar com os arquivos CSV do dataset:
-https://www.unb.ca/cic/datasets/url-2016.html#:~:text=Benign%20URLs%3A%20Over%2035%2C300%20benign,duplicate%20and%20 domain%20only%20URLs
 """
 import sys
 import pandas
 import matplotlib.pyplot as plt
 
 
-MANUALLY_SELECTED_ATT = [
-    'domain_token_count',
-    'NumberofDotsinURL',
-    'Arguments_LongestWordLength',
-    'NumberRate_Domain',
-    'NumberRate_FileName',
-    'NumberRate_AfterPath',
-    'Entropy_Domain',
-    'URL_Type_obf_Type'
-]
-
-
-
 def create_distribution_graph(csv_file, class_column_name):
-    
+    """
+    Plots a barh graph with the class distribution of a csv file dataset
+
+    :param str csv_file: path to the csv file
+    :param class_column_name: the name of the csv column with the class labels
+    """
+
+
     # Reads CSV file with data
     csv_data = pandas.read_csv(csv_file)
 
-    #Create class name array and class count vectors
+    #Create class name/label array and class count vectors
     class_names = csv_data[class_column_name].unique()
     class_counts = csv_data[class_column_name].value_counts()
 
@@ -44,20 +35,21 @@ def create_distribution_graph(csv_file, class_column_name):
     plt.show()
 
 
-def do_main(csv_file):
+def do_main(csv_file, class_column_name):
 
     create_distribution_graph(
         csv_file=csv_file,
-        class_column_name='URL_Type_obf_Type',
+        class_column_name=class_column_name,
     )
 
 
 
 if __name__ == '__main__':
 
-    if len(sys.argv) < 2:
-        exit("Usage: create_distribution_graph.py <csv_file>")
+    if len(sys.argv) < 3:
+        exit("Usage: create_distribution_graph.py <csv_file> <class_column_name>")
     
     do_main(
-        csv_file=sys.argv[1]
+        csv_file=sys.argv[1],
+        class_column_name=sys.argv[2]
     )
